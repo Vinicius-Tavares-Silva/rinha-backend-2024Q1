5eb67b144d34c880e641a4657289f0d3f5780a01
Vinicius-Tavares-Silva <vini_tavares10@hotmail.com>
2024-03-26 18:50:03 -0300

    create docker

--- src/sql_app/database.py
+++ src/sql_app/database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:vini9800@localhost/rinha_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:vini9800@localhost:5432/rinha_db"
 
+print(f'tentando conectar no banco {SQLALCHEMY_DATABASE_URL}')
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
